7bdc2e0f7e1e7697b30545b35a9663365a44ec53
Yuda Setyo Hidayat <yudasetyo07@gmail.com>
2024-12-05 18:30:45 +0700

    Adding Order Logic And Update Stock Logic

---
+++ app/Models/OrderDt.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderDt extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'order_hd_id',
+        'product_id',
+        'total',
+        'price'
+    ];
+
+    /**
+     * Get all of the orderHD for the OrderDt
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orderHD(): HasMany
+    {
+        return $this->hasMany(OrderHd::class);
+    }
+
+    /**
+     * Get all of the products for the OrderDt
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
