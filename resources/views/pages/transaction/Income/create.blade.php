23a0ba18c4bf041dd0f710322d0220669d4a0f93
Yuda Setyo Hidayat <yudasetyo07@gmail.com>
2024-12-30 23:20:34 +0700

    Adding: Error Alert For Income & Outcome

--- resources/views/pages/transaction/Income/create.blade.php
+++ resources/views/pages/transaction/Income/create.blade.php
@@ -28,6 +28,17 @@
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
+                        <!-- Display validation errors -->
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        
                         <form action="{{ route('income') }}" method="POST">
                           @csrf
                           <div class="row">
