1bdd7f9aa88669fe6d0a834a71f6615cf871eed4
siplancelot <sip.lancelot@gmail.com>
2025-01-02 00:18:56 +0700

    fiks conflict

--- resources/views/pages/report/income.blade.php
+++ resources/views/pages/report/income.blade.php
@@ -222,8 +222,6 @@
       borderColor: dataset.borderColor,
       borderWidth: 1
   }));
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const ctx2 = document.getElementById('myChart2').getContext('2d');
 
   const myChart = new Chart(ctx, {
     type: 'bar', // Bar chart
@@ -245,40 +243,6 @@
       }
     }
   });
-    const myChart = new Chart(ctx, {
-        type: 'bar', // Bar chart
-        data: {
-            labels: ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'], // X-axis labels
-            datasets: [{
-                    label: 'Penjualan', // First set of bars
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)', // Bars' fill color
-                    borderColor: 'rgba(255, 99, 132, 1)', // Bars' border color
-                    borderWidth: 1
-                },
-                {
-                    label: 'Lain-lain', // Second set of bars
-                    data: [8, 11, 7, 6, 8, 10],
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
-                }
-            ]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top' // Place legend above the chart
-                }
-            },
-            scales: {
-                y: {
-                    beginAtZero: true // Ensure y-axis starts at 0
-                }
-            }
-        }
-    });
 
 
   const myChart2 = new Chart(ctx2, {
@@ -319,43 +283,7 @@
       }
     }
   });
-    const myChart2 = new Chart(ctx2, {
-        type: 'pie', // Bar chart
-        data: {
-            labels: ['Makanan', 'Minuman', 'Lain-lain'], // X-axis labels
-            datasets: [{
-                    label: 'Produk', // First set of bars
-                    data: [100, 40, 20],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.8)', // Colors for the slices
-                        'rgba(54, 162, 235, 0.8)',
-                        'rgba(255, 206, 86, 0.8)'
-
-                    ],
-                    borderColor: [
-                        'rgba(255, 255, 255, 1)', // Border color for the slices
-                        'rgba(255, 255, 255, 1)',
-                        'rgba(255, 255, 255, 1)'
-                    ],
-                    borderWidth: 1
-                },
-
-            ]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top' // Place legend above the chart
-                }
-            },
-            scales: {
-                y: {
-                    beginAtZero: true // Ensure y-axis starts at 0
-                }
-            }
-        }
-    });
+   
 
 </script>
 
